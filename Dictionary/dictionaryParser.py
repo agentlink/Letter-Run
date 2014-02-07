221b8d29658ac09e3b049b156fd68da405d28222
Gabriel Nicholas <gabe.nicholas@gmail.com>
2014-02-06 23:39:58 -0800

    Figured out how to find legit Qu ocurrences

--- Dictionary/dictionaryParser.py
+++ Dictionary/dictionaryParser.py
@@ -1,24 +1,32 @@
 fileR = open('scrabbleDictionary1.txt', 'r+')
 print("Finished opening file...")
 
-minLength = 3
-maxLength = 7
+min = 3
+max = 7
+minLength = 5 # includes two for new line char
+maxLength = 9 # includes two for new line char
 
 # Create the empty word lists
 wordLists = []
-for n in range (minLength, maxLength + 1):
+for n in range (min, max + 1):
 	wordLists.append([])
 
 for line in fileR:
-	wordLength = len(line)
+	wordLength = len(line) 
 	#The +2 accounts for the \n character
-	if wordLength >= minLength + 2 and wordLength <= maxLength + 2:
-		currentList = wordLists[wordLength - (minLength + 2)]
+	if wordLength >= minLength and wordLength <= maxLength:
+		currentList = wordLists[wordLength - minLength]
 		currentList.append(line)
+	else:
+		quLen = wordLength - line.count("QU")
+		if quLen <= maxLength and wordLength >= minLength and wordLength == maxLength + 2:		
+			#currentList = wordLists[wordLength - (minLength + endCharLen)]
+			#currentList.append(line)
+			print line 
 fileR.close()
 print("Finished loading words into arrays...")
 
-fileW = open('organizedDict.txt', 'w')
+fileW = open('organizedDict_temp.txt', 'w')
 for list in wordLists:
 	for word in list:
 		fileW.write(word)
