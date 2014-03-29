ae2f3a6b4908232f5c3ed25510565c4f87a0321f
gajeam <gabe.nicholas@gmail.com>
2014-03-28 17:13:47 -0700

    Enabled portrait mode

---
+++ Letter Go/LRButtonSection.h
@@ -0,0 +1,13 @@
+//
+//  LRButtonSection.h
+//  Letter Go
+//
+//  Created by Gabriel Nicholas on 3/28/14.
+//  Copyright (c) 2014 Gabe Nicholas. All rights reserved.
+//
+
+#import "LRScreenSection.h"
+
+@interface LRButtonSection : LRScreenSection
+
+@end
---
+++ Letter Go/LRButtonSection.m
@@ -0,0 +1,13 @@
+//
+//  LRButtonSection.m
+//  Letter Go
+//
+//  Created by Gabriel Nicholas on 3/28/14.
+//  Copyright (c) 2014 Gabe Nicholas. All rights reserved.
+//
+
+#import "LRButtonSection.h"
+
+@implementation LRButtonSection
+
+@end
